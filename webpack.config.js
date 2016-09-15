e882ffc224fbdd9a96c881db1c59cfdf140b7a09
jackysee <jackysee@gmail.com>
2016-09-15 10:27:52 +0800

    change folder name to match gh page

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         ]
     },
     output: {
-        path: path.resolve(__dirname + "/doc"),
+        path: path.resolve(__dirname + "/docs"),
         filename: '[name].js'
     },
     module:{
